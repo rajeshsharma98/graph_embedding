7dea6d79a3b912add68f9e3511c76fd9a2dc7265
Pranav Acharya <apranav.acharya@gmail.com>
2022-04-20 19:47:05 -0400

    added util for exporting to pickle file

--- algorithms/deep_walk.py
+++ algorithms/deep_walk.py
@@ -2,6 +2,7 @@ from .embedding_algorithm import Embedding
 from walks.factory import walker
 from .word2Vec import word2VecEmbedding
 from utils import adj_list_reader
+from utils import export_pickel
 
 class DeepWalk(Embedding):
 
@@ -21,10 +22,15 @@ class DeepWalk(Embedding):
         self.embedding = word2VecEmbedding(walks)
 
     def get_embedding(self):
-        return self.embedding
-
+        data = {}
+        for node in self.adj_list:
+            data[node] = self.embedding.wv[node]
+        file_name = "deep_walk_" + self.walker + "_" + str(self.walks) + "_" + str(self.walk_len)
+        export_pickel(data, file_name)
 
 def deep_walk(args):
     print("running deep walk...")
     model = DeepWalk(args)
-    return model.get_embedding()
+    print("exporting embedding...")
+    model.get_embedding()
+    return
