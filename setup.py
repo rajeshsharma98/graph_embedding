a8145b1837572ecba38ea2f494b1c89ce1d3b1c2
Pranav Acharya <apranav.acharya@gmail.com>
2022-04-23 03:17:04 -0400

    incrementing version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gembed',
-    version='1.0.0',    
+    version='1.0.1',    
     description='Graph Embedding Package',
     url='https://github.com/pranavacharya/graph_embedding',
     author='Pranav Acharya',
